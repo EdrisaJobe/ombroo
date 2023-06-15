ee3fa9c3897993a3d67626e1efac3dd39d378df8
edjob <ed.jobe@yahoo.com>
2023-06-14 22:36:10 -0400

    clear button hotfix

--- static/js/clearImplement.js
+++ static/js/clearImplement.js
@@ -1,20 +1,30 @@
 function clearLocalStorage() {
   var implementContainer = document.querySelector(".implement-container");
-  var basicItems = document.querySelectorAll(".basic-container");
+  var verticalContainers = document.querySelectorAll(".vertical-container1");
 
-  // Remove items from the "implement-container"
-  implementContainer.innerHTML = "";
+  // Move items from the "implement-container" back to the last "vertical-container1"
+  while (implementContainer.firstChild) {
+    var item = implementContainer.firstChild;
 
-  // Show all items in the "basic-container"
-  basicItems.forEach(function (basicItem) {
-    basicItem.style.display = "block";
+    // Find the last vertical container
+    var lastVerticalContainer =
+      verticalContainers[verticalContainers.length - 1];
+
+    // Move the item to the last vertical container
+    lastVerticalContainer.appendChild(item);
+  }
+
+  // Show all items in the "vertical-container1"
+  verticalContainers.forEach(function (container) {
+    var items = container.querySelectorAll(".implement-container > p");
+    items.forEach(function (item) {
+      item.style.display = "block";
+    });
   });
 
   // Clear the "implementPositions" key from localStorage
   localStorage.removeItem("implementPositions");
-}
 
-// Load positions when the page loads
-window.addEventListener("DOMContentLoaded", function () {
-  loadPositions();
-});
+  // Refresh the page
+  location.reload();
+}
