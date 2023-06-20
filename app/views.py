96770211d90306ced0cc778f1d00eea223726c35
Edrisa Jobe <edrisa.jobe@its.ny.gov>
2023-06-20 12:00:49 -0400

    store sessions

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from .forms import RegisterForm
+from django.contrib.auth import get_user
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.sessions.backends.db import SessionStore
 
 # Create your views here.
 def home(request):
@@ -10,23 +12,36 @@ def home(request):
 # Workspace - create task
 @login_required
 def workspace(request):
-    tasks = request.session.get('tasks', [])
+    # Get the current user
+    user = get_user(request)
+
+    # Retrieve or create a session object for the user
+    session = request.session
+
+    if 'tasks' not in session:
+        session['tasks'] = []
+
+    tasks = session['tasks']
 
     if request.method == 'POST':
         task = request.POST.get('task')
 
         if task:
             tasks.append(task)
-            request.session['tasks'] = tasks
+            session['tasks'] = tasks
+            session.modified = True
+
             return redirect('workspace')
 
     return render(request, 'pages/workspace.html', {'tasks': tasks})
 
+
 # Workspace - delete specific task
 def delete_task(request, task_index):
-    tasks = request.session.get('tasks', []) # receive tasks from session
+    # Retrieve the tasks from the user's session
+    tasks = request.session.get('tasks', [])
 
-    if task_index >= 0 and task_index < len(tasks): # check range (items in list) and remove using pop
+    if task_index >= 0 and task_index < len(tasks):
         tasks.pop(task_index)
         request.session['tasks'] = tasks
 
@@ -35,6 +50,8 @@ def delete_task(request, task_index):
         return redirect(referring_url)
     else:
         return redirect('workspace')
+
+
     
 #Workspace - delete completed tasks
 def delete_completed_tasks(request):
@@ -46,7 +63,6 @@ def delete_completed_tasks(request):
     return redirect('workspace')
 
 
-# Registration
 def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
@@ -54,8 +70,16 @@ def register(request):
         if form.is_valid():
             form.save()
 
+            # Create a new session for the user
+            session = SessionStore()
+            session.create()
+
+            # Associate the session with the user
+            request.session = session
+
         return redirect('workspace')
     else:
         form = RegisterForm()
         
     return render(request, 'registration/register.html', {"register_form": form})
+
